4363e8adc0a651ef0b3971454f942a5787be43ec
mrfreezee <138147783+mrfreezee@users.noreply.github.com>
2024-05-12 22:22:25 +0300

    Update index.tsx
    4

--- src/index.tsx
+++ src/index.tsx
@@ -12,9 +12,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Provider store={appStore}>
-      <Main />
-    </Provider>
+    <Main/>
   </React.StrictMode>
 );
 
