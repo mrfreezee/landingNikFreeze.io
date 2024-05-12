9b781fb5cb2dcc29162a448f0a3ab5c582f3613c
mrfreezee <138147783+mrfreezee@users.noreply.github.com>
2024-05-12 22:19:40 +0300

    Update index.tsx
    four commit

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { appStore } from './store/store';
+import { Main } from './components/main/main';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={appStore}>
-      <App />
+      <Main />
     </Provider>
   </React.StrictMode>
 );
