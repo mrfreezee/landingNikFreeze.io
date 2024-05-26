a71c845e61f223a518de1dc919f494beed0695c3
mrfreezee <138147783+mrfreezee@users.noreply.github.com>
2024-05-26 17:39:38 +0300

    comm19

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -217,10 +217,11 @@ export const Main = () => {
             </div>
             <div className={theme === 'dark' ? 'section4' : 'section4 section4-theme'}>
             <div className={theme === 'dark' ? 'backgr4' : 'backgr4 backgr4-light'}></div>
+            <img src='https://i.ibb.co/qNPjv3P/gray-abstract-digital-art-uhdpaper-com-4-K-4-328.jpg' className='video'/>
                 {/* <video className='video' autoPlay loop muted playsInline>
                         <source src="https://cdn.pixabay.com/video/2023/03/09/153957-806571952_large.mp4" type="video/mp4" />
                     </video> */}
-                    <video className='video' role="application" id="vjs_video_3_html5_api"  autoPlay loop muted playsInline webkit-playinginline src="https://cdn.pixabay.com/video/2023/03/09/153957-806571952_large.mp4"></video>
+                    {/* <video className='video' role="application" id="vjs_video_3_html5_api"  autoPlay loop muted playsInline webkit-playinginline src="https://cdn.pixabay.com/video/2023/03/09/153957-806571952_large.mp4"></video> */}
                 <div className='section-container container4'>
                     <div className='sect4-text'>
                         Лендинги
